7992810ed0201e7127835cd73ad139c5c8ee8fd9
Murad <engno_95@mail.ru>
2024-01-22 12:39:30 +0300

    добавлена ViewModel и вынесена логика изменения состояние кнопки во ViewModel

--- app/src/main/java/com/muslim/simpleappcompose/InstagramProfileCard.kt
+++ app/src/main/java/com/muslim/simpleappcompose/InstagramProfileCard.kt
@@ -1,10 +1,10 @@
 package com.muslim.simpleappcompose
 
+import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,11 +14,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,14 +31,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.muslim.simpleappcompose.ui.theme.SimpleAppComposeTheme
 
 
 @Composable
-fun InstagramProfileCard() {
+fun InstagramProfileCard(
+    viewModel: MainViewModel
+) {
+
+    Log.d("RECOMPOSITION", "InstagramProfileCard")
+    val isFollowed = viewModel.isFollowing.observeAsState(false)
 
     Card(
         modifier = Modifier
@@ -48,8 +56,11 @@ fun InstagramProfileCard() {
         ),
         border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.onBackground)
     ) {
-        Column(modifier = Modifier
-            .padding(16.dp)
+
+        Log.d("RECOMPOSITION", "Card")
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
         ) {
 
             Row(
@@ -87,12 +98,40 @@ fun InstagramProfileCard() {
                 fontSize = 14.sp
             )
 
-            Button(onClick = {}) {
-                Text(text = "Follow")
+            FollowButton(isFollowed = isFollowed) {
+                viewModel.changeFollowingStatus()
             }
         }
     }
 
+}
+
+@Composable
+private fun FollowButton(
+    isFollowed: State<Boolean>,
+    clickListener: () -> Unit,
+) {
+
+    Log.d("RECOMPOSITION", "FollowButton")
+    Button(
+        onClick = {
+            clickListener()
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isFollowed.value) {
+                MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+            } else {
+                MaterialTheme.colorScheme.primary
+            }
+        )
+    ) {
+        val text = if (isFollowed.value) {
+            "Unfollow"
+        } else {
+            "Follow"
+        }
+        Text(text = text)
+    }
 }
 
 @Composable
@@ -107,7 +146,7 @@ private fun UserStatistics(
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
         Text(
-           text = value,
+            text = value,
             fontSize = 24.sp,
             fontFamily = FontFamily.Cursive
         )
@@ -119,23 +158,23 @@ private fun UserStatistics(
     }
 }
 
-@Preview
-@Composable
-fun PreviewCardLight() {
-
-    SimpleAppComposeTheme(
-        darkTheme = false
-    ) {
-        InstagramProfileCard()
-    }
-}
-
-@Preview
-@Composable
-fun PreviewCardDark() {
-    SimpleAppComposeTheme(
-        darkTheme = true
-    ) {
-        InstagramProfileCard()
-    }
-}
+//@Preview
+//@Composable
+//fun PreviewCardLight() {
+//
+//    SimpleAppComposeTheme(
+//        darkTheme = false
+//    ) {
+//        InstagramProfileCard()
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun PreviewCardDark() {
+//    SimpleAppComposeTheme(
+//        darkTheme = true
+//    ) {
+//        InstagramProfileCard()
+//    }
+//}
--- app/src/main/java/com/muslim/simpleappcompose/MainActivity.kt
+++ app/src/main/java/com/muslim/simpleappcompose/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
 import com.muslim.simpleappcompose.ui.theme.SimpleAppComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel = ViewModelProvider(this) [MainViewModel::class.java]
         setContent {
             SimpleAppComposeTheme {
                 Box(
@@ -20,7 +22,7 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .background(MaterialTheme.colorScheme.background)
                 ) {
-                    InstagramProfileCard()
+                    InstagramProfileCard(viewModel )
                 }
             }
         }
